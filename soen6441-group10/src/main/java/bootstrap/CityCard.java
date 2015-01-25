a26869a5bbf8553ed5d66165d7fd95cf971c0de9
amirhakim <amirhakim@gmail.com>
2015-01-25 17:00:33 -0500

    CityCard and CityDeck Skeleton Added

---
+++ soen6441-group10/src/main/java/bootstrap/CityCard.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package bootstrap;
+
+/**
+ * @author Amir
+ *
+ */
+public class CityCard implements Card {
+
+	/**
+	 * 
+	 */
+	
+	
+	
+	public CityCard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see bootstrap.Card#getTitle()
+	 */
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see bootstrap.Card#setTitle(java.lang.String)
+	 */
+	public void setTitle(String title) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see bootstrap.Card#clone()
+	 */
+	public Card clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
