5a154b7493d72f41126cc4b92b49ad850b8d1130
Knoparast <Katayoon.noparast@gmail.com>
2015-02-03 07:33:32 -0500

    Card class refined

--- soen6441-group10/src/main/java/card/Card.java
+++ soen6441-group10/src/main/java/card/Card.java
@@ -1,16 +1,23 @@
 package card;
 
 /**
- * <b> This interface represents the contract for all the card types that will be required in the game <b> 
- * every respective class MUST implement it.
+ * <b> This interface represents the contract for all the card types that will be required in the game. <b> 
+ * Each respective class MUST implement it.
  * @author Team 10 - SOEN6441
  * @version 1.0
  */
 
 public interface Card extends Cloneable {
 
+	/**
+	 * Gets the title of card AKA name
+	 */
 	public String getTitle();
 
+	/**
+	 * Sets the title of card AKA name
+	 * @param title
+	 */
 	public void setTitle(String title);
 
 }
