f75fea0062d989ab0696d9e9f80a73c0364fd2cc
Knoparast <Katayoon.noparast@gmail.com>
2015-02-04 11:57:06 -0500

    CityCard Comments

--- soen6441-group10/src/main/java/card/CityCard.java
+++ soen6441-group10/src/main/java/card/CityCard.java
@@ -13,7 +13,8 @@ import java.util.Map;
 import util.Color;
 
 /**
- * <b> This class represents the city cards of the game <b>
+ * <b> This class implements the city cards of the game with their related rules of movement and all information relevant
+ *   to that area on the board.<b>
  * 
  * @author Team 10 - SOEN6441
  * @version 1.0
@@ -29,6 +30,9 @@ public class CityCard implements Card {
 	private int trolls;
 	private int buildingCost;
 
+	/**
+	 * This constructor is invoked to create objects from the class CityCard.
+	 */
 	public CityCard() {
 		this.neighbours = new ArrayList<CityCard>();
 		this.minions = new HashMap<Color, Integer>();
@@ -37,23 +41,41 @@ public class CityCard implements Card {
 		this.demons = 0;
 		this.trolls = 0;
 	}
-
+/**
+ * This method takes title and building cost of city cards.
+ * @param title_ The title of the city card
+ * @param buildingCost_ The building cost of that area
+ */
 	public CityCard(String title_, int buildingCost_) {
 		this();
 		title = title_;
 		buildingCost = buildingCost_;
 	}
 
+	/**
+	 * This method implements getTitle method of interface Card. 
+	 * It gets title of city card.
+	 */
 	@Override
 	public String getTitle() {
 		return this.title;
 	}
 
+	/**
+	 * This method implements setTitle method of interface Card. 
+	 * It sets title of city card.
+	 */
 	@Override
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * This method adds the neighbours of the city area.
+	 * @param neighbourCard the neighbour of the city area
+	 * @param recipricate sets true if it is adjacent to this area
+	 * @return adjacent city areas
+	 */
 	public CityCard addNeighbour(CityCard neighbourCard, boolean recipricate) {
 		this.neighbours.add(neighbourCard);
 		if (recipricate)
@@ -61,14 +83,27 @@ public class CityCard implements Card {
 		return this;
 	}
 
+	/**
+	 * This method checks to see the neighbour of the city area.
+	 * @param card2 the city area
+	 * @return true if that is adjacent to the city area
+	 */
 	public boolean isNeighbour(CityCard card2){
 		return this.neighbours.contains(card2);
 	}
 
+	/**
+	 * This method checks to see if there is a trouble marker in the city area.
+	 * @return true if there is a trouble market in the city area
+	 */
 	public boolean hasTroubleMaker() {
 		return troubleMaker;
 	}
 
+	/**
+	 * This method specifies the building of the player as building owner.
+	 * @return true if the building belongs to the player
+	 */
 	public Player getBuilding() {
 		if (this.buildingOwner == null) {
 			return null;
@@ -77,6 +112,11 @@ public class CityCard implements Card {
 		}
 	}
 
+	/**
+	 * This method makes building for the player as building owner in case there is not any building in the city area.
+	 * @param p player as the building owner
+	 * @return true if the player makes a building successfully in the city area
+	 */
 	public boolean setBuilding(Player p) {
 		if (this.buildingOwner == null) {
 			this.buildingOwner = p;
@@ -87,6 +127,11 @@ public class CityCard implements Card {
 		}
 	}
 
+	/**
+	 * This method determines the number of minions of the player 
+	 * @param p the player
+	 * @return number of the minions of the player
+	 */
 	public int numberOfMinions(Player p) {
 		if (this.minions.get(p) == null) {
 			return 0;
@@ -95,10 +140,19 @@ public class CityCard implements Card {
 		}
 	}
 
+	/**
+	 * This method maps the minions of each player with a color.
+	 * @return minions mapped to a color
+	 */
 	public Map<Color, Integer> getMinions() {
 		return minions;
 	}
 
+	/**
+	 * This method adds the minion of the player in the city area,
+	 *  and justifies the balance of stocked minions.
+	 * @param p the player
+	 */
 	public void addMinion(Player p) {
 		Color playerColor = p.getColor();
 		p.decreaseMinion();
@@ -110,6 +164,11 @@ public class CityCard implements Card {
 		}
 	}
 
+	/**
+	 * This method removes the minion of the player from the city area,
+	 *  and justifies the balance of stocked minions.
+	 * @param p the player
+	 */
 	public void removeMinion(Player p) {
 		if (this.minions.get(p.getColor()) == null) {
 			return;
