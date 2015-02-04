1d1e2d5d04b7514cceb2f835e7c57a12e813bac2
Knoparast <Katayoon.noparast@gmail.com>
2015-02-04 17:31:18 -0500

    Modification on Comments

--- soen6441-group10/src/main/java/card/PersonalityCard.java
+++ soen6441-group10/src/main/java/card/PersonalityCard.java
@@ -1,23 +1,37 @@
-/**
- * As the name suggests, this class represents game 
- * personalities (Lord Vetinaari etc.).
- */
+
 package card;
 
+/**
+ * <b> This class implements the Personality cards of the game with their related rules<b>
+ * 
+ * @author Team 10 - SOEN6441
+ * @version 1.0
+ */
 public class PersonalityCard implements Card {
 
 	private String title;
-
+	
+	/**
+	 * This method implements getTitle method of interface Card. 
+	 * It gets the title of personality card.
+	 */
 	@Override
 	public String getTitle() {
 		return this.title;
 	}
 
+	/**
+	 * This method implements setTitle method of interface Card. 
+	 * It sets the title of personality card.
+	 */
 	@Override
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -48,9 +62,13 @@ public class PersonalityCard implements Card {
 		return true;
 	}
 
+	/**
+	 * This method overrides implementation of toString in a way to print the title of the personality card.
+	 */
 	@Override
 	public String toString() {
 		return "PersonalityCard[ " + title + " ]";
 	}
 
 }
+
