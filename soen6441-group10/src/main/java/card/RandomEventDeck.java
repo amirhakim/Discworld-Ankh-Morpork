a704e81543b54d8ad65a38cffacdcb5f16930773
Knoparast <Katayoon.noparast@gmail.com>
2015-02-04 18:50:41 -0500

    Modification on RandomEventDeck

--- soen6441-group10/src/main/java/card/RandomEventDeck.java
+++ soen6441-group10/src/main/java/card/RandomEventDeck.java
@@ -1,31 +1,38 @@
-/**
- * 
- */
+
 package card;
 
 import java.util.Stack;
 
 /**
- * @author Amir
- *
+ * This class implements deck of random event cards of the game. 
+ * 
+ * @author Team 10 - SOEN6441
+ * @version 1.0
  */
 public class RandomEventDeck extends Deck<RandomEventCard> {
 
-	/**
-	 * 
-	 */
+	
 	private final int size = 12;
 
-	// Array of all card posibilities
+	/**
+	 *  Array of all card possibilities
+	 */
 	private final String[] names = { "The Dragon", "Flood", "Fire", "Fog",
 			"Riots", "Explosion", "Mysterious Murders",
 			"Demons from the Dungeon Dimensions", "Subsidence",
 			"Bloody Stupid Johnson", "Trolls", "Earthquake" };
 
+	/**
+	 * This constructor is invoked from the class RandomEventDeck.
+	 */
 	public RandomEventDeck() {
 		populateDeck();
 	}
 
+	/**
+	 * This method overrides "populateDeck" of super class Deck and
+	 *  puts all the random event cards in the deck and shuffle it.
+	 */
 	@Override
 	public void populateDeck() {
 		cards = new Stack<RandomEventCard>();
