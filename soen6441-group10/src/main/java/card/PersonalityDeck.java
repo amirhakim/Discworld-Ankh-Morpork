6be7b8f1aa2c0f24ca907030f184222301347176
Knoparast <Katayoon.noparast@gmail.com>
2015-02-04 18:49:39 -0500

    Modification on PersonalityDeck

--- soen6441-group10/src/main/java/card/PersonalityDeck.java
+++ soen6441-group10/src/main/java/card/PersonalityDeck.java
@@ -1,8 +1,9 @@
 /**
- * @File
- * Class continaing all personality cards
+ * This class implements deck of personality cards of the game. 
+ * 
+ * @author Team 10 - SOEN6441
+ * @version 1.0
  */
-
 package card;
 
 import java.util.Stack;
@@ -11,14 +12,23 @@ public class PersonalityDeck extends Deck<PersonalityCard> {
 
 	private final int size = 5;
 
-	// Array of all card posibilities
+	/**
+	 *  Array of all card possibilities
+	 */
 	private final String[] names = { "Lord Selachii", "Chrysoprase",
 			"Lord Vetinaari", "Dragon King of Arms", "Commander Vimes" };
 
+	/**
+	 * This constructor is invoked from the class PersonalityDeck.
+	 */
 	public PersonalityDeck() {
 		populateDeck();
 	}
-
+	
+	/**
+	 * This method overrides "populateDeck" of super class Deck and
+	 *  puts all the personality cards in the deck and shuffle it.
+	 */
 	@Override
 	public void populateDeck() {
 		this.cards = new Stack<PersonalityCard>();
